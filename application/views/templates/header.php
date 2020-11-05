fc471a10fee6b9fa66622e7842f7bbc9d422e3a5
mhafizzamrudin <hafizzamrudin92@gmail.com>
2020-11-05 20:12:55 +0700

    added features tambah mahasiswa

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -19,7 +19,7 @@
 		  </button>
 		  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 		    <div class="navbar-nav">
-		      <a class="nav-link" href="<?php base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+		      <a class="nav-link" href="<?php base_url(); ?>home">Home <span class="sr-only">(current)</span></a>
 		      <a class="nav-link" href="<?php base_url(); ?>mahasiswa">Mahasiswa</a>
 		      <a class="nav-link" href="#">About</a>
 		    </div>
